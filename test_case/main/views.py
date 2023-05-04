cc18230a1f4d090fc44baa8a3fc5e1ce62c91561
Alex <alex.ter.61660z2@gmail.com>
2023-05-05 02:51:31 +0500

    added path('about') in urls.py in 'main'app and added about() in views.py

--- test_case/main/views.py
+++ test_case/main/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h4>Проверка работы</h4>")
+
+
+def about(request):
+    return HttpResponse("<h4>Страница о нас</h4>")
+
+
