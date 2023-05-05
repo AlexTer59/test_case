43d6b1061ffe1f2ffea1e412a65540a3420d9a68
Alex <alex.ter.61660z2@gmail.com>
2023-05-05 12:27:01 +0500

    added about.html and location.html

--- test_case/main/views.py
+++ test_case/main/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
 
 
 def about(request):
-    return HttpResponse("<h4>Страница о нас</h4>")
+    return render(request, 'main/about.html')
 
 
 def location(request):
-    return HttpResponse("<h4>Мы тут</h4>")
+    return render(request, 'main/location.html')
