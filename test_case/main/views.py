109406ac35be6c9e7fb5414c47ceea2d366871e8
Alex <alex.ter.61660z2@gmail.com>
2023-05-08 16:11:52 +0500

    added new app news

--- test_case/main/views.py
+++ test_case/main/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
 # Create your views here.
 def index(request):
-    data = {'title': 'Главная страница!!',
-            'values': ['Some', 'Hello', '123'],
-            'obj': {
-                'car': 'BMW',
-                'age': 18,
-                'hobby': 'football'
-                }
-            }
+    data = {'title': 'Главная страница!!'}
     return render(request, 'main/index.html', data)
 
 
