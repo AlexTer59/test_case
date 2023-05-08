6f61f24d019dc6b4df2ddf1e784679d230104d9a
Alex <alex.ter.61660z2@gmail.com>
2023-05-08 15:32:07 +0500

    added list values in index() and index.html

--- test_case/main/views.py
+++ test_case/main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 # Create your views here.
 def index(request):
-    data = {'title': 'Главная страница!!'}
+    data = {'title': 'Главная страница!!',
+            'values': ['Some', 'Hello', '123']
+            }
     return render(request, 'main/index.html', data)
 
 
