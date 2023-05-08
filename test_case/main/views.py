553f4c0f93b45014f2d581d047df0940c4279f0f
Alex <alex.ter.61660z2@gmail.com>
2023-05-08 15:44:48 +0500

    added obj in index.html and index()

--- test_case/main/views.py
+++ test_case/main/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     data = {'title': 'Главная страница!!',
-            'values': ['Some', 'Hello', '123']
+            'values': ['Some', 'Hello', '123'],
+            'obj': {
+                'car': 'BMW',
+                'age': 18,
+                'hobby': 'football'
+                }
             }
     return render(request, 'main/index.html', data)
 
