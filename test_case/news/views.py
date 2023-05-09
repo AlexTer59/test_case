854ecd61252243e90ee055250351219a7236d081
Alex <alex.ter.61660z2@gmail.com>
2023-05-09 19:55:46 +0500

    added create.html

--- test_case/news/views.py
+++ test_case/news/views.py
@@ -4,4 +4,8 @@ from .models import Articles
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+def create(request):
+    return render(request, 'news/create.html')
 # Create your views here.
