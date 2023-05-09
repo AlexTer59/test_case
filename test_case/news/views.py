14b1f49234b2a0b9e6ef4142497fdb24dac295d0
Alex <alex.ter.61660z2@gmail.com>
2023-05-09 20:54:54 +0500

    added the form for adding news in database

--- test_case/news/views.py
+++ test_case/news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
@@ -7,5 +8,21 @@ def news_home(request):
 
 
 def create(request):
-    return render(request, 'news/create.html')
+    error = ''
+    if request.method == "POST":
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+        else:
+            error = 'Форма была неверной'
+
+
+
+    form = ArticlesForm()
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'news/create.html', data)
 # Create your views here.
